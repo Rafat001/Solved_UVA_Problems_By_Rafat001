6ed9538215d7876ef27526a9d64a3d5b651ec7d8
Majharul Islam Rafat <rafatislam.syl@gmail.com>
2016-10-17 21:10:52 +0600

    Update UVA 374.java

--- UVA 374.java
+++ UVA 374.java
@@ -5,13 +5,13 @@ public class Main {
 	{
 		Scanner scan=new Scanner(System.in);
 		BigInteger base,md,power,ans;
-		while(scan.hasNext())
+		while(scan.hasNext()) // For scanning until EOF //
 		{
 			ans=new BigInteger("0");
 			base=scan.nextBigInteger();
 			power=scan.nextBigInteger();
 			md=scan.nextBigInteger();
-			ans=base.modPow(power, md);
+			ans=base.modPow(power, md); // it means ans=(base^power)%mod //
 			System.out.println(ans);
 		}
 	}
