885b3016b6187ec24f3c0b98c626fe322ce6e73d
Majharul Islam Rafat <rafatislam.syl@gmail.com>
2016-11-10 18:20:18 +0600

    Update UVA 10392.cpp

--- UVA 10392.cpp
+++ UVA 10392.cpp
@@ -1,46 +1,48 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#include<math.h>
+#define maxx 1000005
+long long prime[maxx],pp[maxx];
 int main()
 {
-    long long i,flag,n,limit,gg;
+    long long n,i;
+    int l=sqrt(maxx),j;
+    prime[0]=prime[1]=1;
+    for(i=4; i<maxx; i+=2)
+    {
+        prime[i]=1;
+    }
+    for(i=3; i<=l; i+=2)
+    {
+        if(prime[i]==0)
+        {
+            for(j=i*i; j<maxx; j+=i)
+            {
+                prime[j]=1;
+            }
+        }
+    }
+    int k=0;
+    for(i=2;i<maxx;i++)
+    {
+        if(prime[i]==0)
+            pp[k++]=i;
+    }
     while(scanf("%lld",&n)==1)
     {
-        queue<long long>list;
-        flag=0;
         if(n<0)
             break;
-        limit=sqrt(n);
-        for(i=2; i<=limit; i++)
+        for(i=0; i<k; i++)
         {
-            if(n%i==0)
+            while(n%pp[i]==0)
             {
-                flag=1;
-                while(n%i==0)
-                {
-                    list.push(i);
-                    n=n/i;
-                }
+                printf("    %lld\n",pp[i]);
+                n=n/pp[i];
             }
-            if(flag==1 && n!=1)
-                gg=1;
             if(n==1)
-                {
-                    gg=0;
-                    break;
-                }
+                break;
         }
-        if(flag==0)
-            {
-                list.push(n);
-                gg=0;
-            }
-        if(gg==1)
-            list.push(n);
-        while(!list.empty())
-        {
-            printf("    %lld\n",list.front());
-            list.pop();
-        }
+        if(n!=1)
+            printf("    %lld\n",n);
         printf("\n");
     }
     return 0;
